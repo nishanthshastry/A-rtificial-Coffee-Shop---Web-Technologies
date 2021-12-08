36c38c553067b807ea4f0649c873cb0d5c40936e
nishanthshastry <62718714+nishanthshastry@users.noreply.github.com>
2021-12-08 20:40:17 +0530

    Delete code directory

--- code/bootstrap/ReadMe.md
+++
@@ -1,3 +0,0 @@
-# Note regarding the contents of this file
-
-**Its important to note that the *bootstrap* file has the required *css*, *js* and *fonts* that are majorly concerning this project.**
--- code/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","<username>","<password>","shop") or die(mysqli_error($con));
-//$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
-?>
